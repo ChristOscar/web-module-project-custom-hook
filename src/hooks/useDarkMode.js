e8f3ce04ecd2b5fc79e49f03dd20dec641d8e35c
Christwide Oscar <triple101.games@gmail.com>
2022-01-12 15:09:39 -0600

    Everything works having an isseu displaying the value of the booloen

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import useLocalStorage from "./useLocalStorage";
+
+
+ const useDarkMode = (initialValue) => {
+    const [someValue, setSomeValue] = useLocalStorage('values', initialValue)
+    return [someValue, setSomeValue];
+ }
+ export default useDarkMode
